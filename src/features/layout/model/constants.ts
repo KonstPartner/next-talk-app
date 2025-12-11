d0a062403a445c9bc31a6334ea3b12468938702b
KonstPartner <konst.partnership@gmail.com>
2025-12-11 00:47:03 -0800

    feat(ssr-page): ssr posts page

--- src/features/layout/model/constants.ts
+++ src/features/layout/model/constants.ts
@@ -1,5 +1,6 @@
 export const NAV_LINKS = [
   { href: '/', label: 'Home' },
+  { href: '/preview-posts', label: 'Preview Posts' },
   { href: '/posts/create', label: 'Create' },
   { href: '/profile', label: 'Profile' },
 ];
